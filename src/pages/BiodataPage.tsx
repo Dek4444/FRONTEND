23202a2764920452dde66d99d5d4ac04883e5d57
Dek4444 <aureliodeca1955@gmail.com>
2026-05-23 16:07:08 +0700

    update biodata social media

--- src/pages/BiodataPage.tsx
+++ src/pages/BiodataPage.tsx
@@ -9,19 +9,46 @@ export default function BiodataPage() {
             <td className="w-52 bg-gray-100 p-3 font-semibold">NIM</td>
             <td className="p-3">24090122</td>
           </tr>
+
           <tr className="border-b">
             <td className="bg-gray-100 p-3 font-semibold">Nama</td>
             <td className="p-3">Aurelio Deca Setyawan</td>
           </tr>
+
           <tr className="border-b">
-            <td className="bg-gray-100 p-3 font-semibold">Program Studi</td>
+            <td className="bg-gray-100 p-3 font-semibold">
+              Program Studi
+            </td>
             <td className="p-3">Informatic Engineering</td>
           </tr>
+
           <tr>
-            <td className="bg-gray-100 p-3 font-semibold">Informasi Lainnya</td>
-            <td className="p-3">
-              Website EVENTMS dibuat menggunakan React, TypeScript, Tailwind CSS,
-              Zustand, Express, Prisma, dan Supabase PostgreSQL.
+            <td className="bg-gray-100 p-3 font-semibold">
+              Informasi Lainnya
+            </td>
+
+            <td className="space-y-2 p-3">
+              <div>
+                <a
+                  href="https://instagram.com/dekastywn_"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-pink-500 hover:underline"
+                >
+                  Instagram: @dekastywn_
+                </a>
+              </div>
+
+              <div>
+                <a
+                  href="https://github.com/Dek4444"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-600 hover:underline"
+                >
+                  Github: Dek4444
+                </a>
+              </div>
             </td>
           </tr>
         </tbody>
